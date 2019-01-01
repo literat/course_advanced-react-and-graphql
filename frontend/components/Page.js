6a295125ee81522c29e348abe0569e2fb8e0665b
literat <tomas@litera.me>
2019-01-01 21:20:37 +0100

    An Intro to Styled Components

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Meta from './Meta';
+import styled from 'styled-components';
+
+const MyButton  = styled.button`
+    background: red;
+    font-size: ${props => (props.huge ? '100px' : '50px')};
+    .poop {
+        font-size: 100px;
+    }
+`;
 
 class Page extends Component {
     render () {
@@ -8,6 +17,14 @@ class Page extends Component {
             <div>
                 <Meta />
                 <Header />
+                <MyButton huge>
+                    Click Me
+                    <span className='poop'>💩</span>
+                </MyButton>
+                <MyButton>
+                    Click Me
+                    <span className='poop'>💩</span>
+                </MyButton>
                 {this.props.children}
             </div>
         )
