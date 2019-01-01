ba3f9bd70d2b0043fc956ccacc353ae33b6ed474
literat <tomas@litera.me>
2019-01-01 20:59:12 +0100

    fix: Typo

--- frontend/components/Meta.js
+++ frontend/components/Meta.js
@@ -4,7 +4,7 @@ const Meta = () => (
     <Head>
         <meta name='viewport' content='width=device-width,initial-scale=1' />
         <meta charset='utf-8' />
-        <link rel='shortcut-icon' href='/static/favicon.png' />
+        <link rel='shortcut icon' href='/static/favicon.png' />
         <link rel='stylesheet' type='text/css' href='/static/nprogress.css' />
         <title>Sick Fits!</title>
     </Head>
