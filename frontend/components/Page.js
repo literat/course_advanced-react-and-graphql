b294570cfcf72df3b8cd0284da16336e3298ec23
literat <tomas@litera.me>
2019-01-07 09:26:44 +0100

    Global Styling and Typography with Styled Components

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -50,6 +50,32 @@ injectGlobal`
   }
 `;
 
+injectGlobal`
+    @font-face {
+        font-family: 'radnika_next';
+        src: url('/static/radnikanext-medium-webfont.woff2') format('woff2');
+        font-weight: normal;
+        font-style: normal;
+    }
+    html {
+        box-sizing: border-box;
+        font-size: 10px;
+    }
+    *, *:before, *:after {
+        box-sizing: inherit;
+    }
+    body {
+        padding: 0;
+        margin: 0;
+        font-size: 1.5rem;
+        line-height: 2;
+    }
+    a {
+        text-decoration: none;
+        color: ${theme.black};
+    }
+`;
+
 class Page extends Component {
   render () {
     return (
