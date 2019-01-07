d7289414510d041a811b7f2f9ae5b1a6683a8260
literat <tomas@litera.me>
2019-01-07 09:29:02 +0100

    fixup! Global Styling and Typography with Styled Components

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -27,8 +27,7 @@ const Inner = styled.div`
 injectGlobal`
     @font-face {
         font-family: 'radnika_next';
-        src: url('/static/radnikanext-medium-webfont.woff2');
-        format('woff2');
+        src: url('/static/radnikanext-medium-webfont.woff2') format('woff2');
         font-weight: normal;
         font-style: normal;
     }
