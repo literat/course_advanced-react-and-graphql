494fe0793548b6458b8bc272dbe84396d77d5fc5
literat <tomas@litera.me>
2019-01-15 22:13:15 +0100

    Our first Query and Mutation

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,12 @@
-const Mutations = {};
+const Mutations = {
+  createDog(parent, args, context, info) {
+    global.dogs = global.dogs || [];
+    // create a dog
+    const newDog = { name: args.name };
+    global.dogs.push(newDog);
+
+    return newDog;
+  }
+};
 
 module.exports = Mutations;
