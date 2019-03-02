f87a0bb323814598d5b6822873cd99ed4452fdc9
literat <tomas@litera.me>
2019-03-02 23:11:03 +0100

    Pagination

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,10 +1,10 @@
 import Link from 'next/link';
 import Items from '../components/Items';
 
-const Home = () => (
+const Home = props => (
   <div>
-    <Items />
+    <Items page={parseFloat(props.query.page) || 1} />
   </div>
-)
+);
 
 export default Home;
