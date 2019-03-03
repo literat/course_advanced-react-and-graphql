525728c6bd5daef5f8bebb96383b76f80ef73fd1
literat <tomas@litera.me>
2019-03-03 21:34:47 +0100

    User Signup and Permission Flow

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,11 @@
+const cookieParser = require('cookie-parser');
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
 const db = require('./db');
 
 const server = createServer();
 
-// TODO Use express middleware to handle cookies (JWT)
+server.express.use(cookieParser());
 // TODO Use express middleware to populate current user
 
 server.start(
@@ -16,5 +17,5 @@ server.start(
   },
   deets => {
     console.log(`Server is now running on http://localhost:${deets.port}`);
-  }
+  },
 );
