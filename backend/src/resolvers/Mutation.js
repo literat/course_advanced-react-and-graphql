55fd65db81d46a30fdd9c0ccf25521edd0f1fb9e
literat <tomas@litera.me>
2019-04-18 14:48:35 +0200

    Sign in Form and Custom Error Handling

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -67,6 +67,27 @@ const Mutations = {
     // Finallllly we return the user to the browser
     return user;
   },
+  async signin(parent, { email, password }, context, info) {
+    // 1. Check if there is a user with that email
+    const user = await context.db.query.user({ where: { email } });
+    if (!user) {
+      throw new Error(`No such user found for email ${email}`);
+    }
+    // 2. Check if their password is correct
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+      throw new Error('Invalid Password!');
+    }
+    // 3. Generate the JWT Token
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // 4. Set the cookie with the token
+    context.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+    });
+    // 5. Return the user
+    return user;
+  },
 };
 
 module.exports = Mutations;
