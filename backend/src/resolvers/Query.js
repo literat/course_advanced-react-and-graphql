ad02bfcdd5de44a7e38a1afdf71176eb15b8dbf6
literat <tomas@litera.me>
2019-04-18 13:58:45 +0200

    Currently Logged In User with Middleware and Render Props

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -4,6 +4,21 @@ const Query = {
   items: forwardTo('db'),
   item: forwardTo('db'),
   itemsConnection: forwardTo('db'),
+  me(parent, args, context, info) {
+    // check if there is a current user ID
+    if (!context.request.userId) {
+      return null;
+    }
+
+    return context.db.query.user(
+      {
+        where: {
+          id: context.request.userId,
+        },
+      },
+      info,
+    );
+  },
 };
 
 module.exports = Query;
