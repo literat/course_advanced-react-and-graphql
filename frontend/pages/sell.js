8a0920cd47cf4105dce2cfbde66da070a5fecd30
literat <tomas@litera.me>
2019-04-26 14:24:45 +0200

    Creating a Gated Sign In Component

--- frontend/pages/sell.js
+++ frontend/pages/sell.js
@@ -1,10 +1,13 @@
 import Link from 'next/link';
 import CreateItem from '../components/CreateItem';
+import PleaseSignIn from '../components/PleaseSignIn';
 
 const Sell = () => (
   <div>
-    <CreateItem />
+    <PleaseSignIn>
+      <CreateItem />
+    </PleaseSignIn>
   </div>
-)
+);
 
 export default Sell;
