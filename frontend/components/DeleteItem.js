a9ff162eee1de1b9dabfa8e1a23a5605740899f7
literat <tomas@litera.me>
2019-04-29 22:23:29 +0200

    Locking Down DeleteItem Permissions

--- frontend/components/DeleteItem.js
+++ frontend/components/DeleteItem.js
@@ -35,7 +35,7 @@ class DeleteItem extends Component {
           <button
             onClick={() => {
               if (confirm('Are you sure you want to delete this item?')) {
-                deleteItem();
+                deleteItem().catch(error => alert(error.message));
               }
             }}
           >
