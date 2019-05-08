7683b92c3df77da7d98ffd86f2716563bf2106ae
literat <tomas@litera.me>
2019-05-08 21:40:39 +0200

    Mocking 101

---
+++ frontend/__tests__/mocking.test.js
@@ -0,0 +1,36 @@
+function Person(name, foods) {
+  this.name = name;
+  this.foods = foods;
+}
+
+Person.prototype.fetchFavFoods = function() {
+  return new Promise((resolve, reject) => {
+    // Simulate an API
+    setTimeout(() => resolve(this.foods), 2000);
+  });
+};
+
+describe('mocking learing', () => {
+  it('mocks a reg function', () => {
+    const fetchDogs = jest.fn();
+    fetchDogs('snickers');
+    expect(fetchDogs).toHaveBeenCalled();
+    expect(fetchDogs).toHaveBeenCalledWith('snickers');
+    fetchDogs('hugo');
+    expect(fetchDogs).toHaveBeenCalledTimes(2);
+    console.log(fetchDogs);
+  });
+
+  it('can create a person', () => {
+    const me = new Person('Wes', ['pizza', 'bugs']);
+    expect(me.name).toBe('Wes');
+  });
+
+  it('can fetch food', async () => {
+    const me = new Person('Wes', ['pizza', 'bugs']);
+    // mock the favFoods function
+    me.fetchFavFoods = jest.fn().mockResolvedValue(['sushi', 'ramen']);
+    const favFoods = await me.fetchFavFoods();
+    expect(favFoods).toContain('sushi');
+  });
+});
