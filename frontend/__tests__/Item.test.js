23f68fe6be94b434c420ad3e72d70df2545e52f4
literat <tomas@litera.me>
2019-05-14 17:57:23 +0200

    First Tests and Shallow Rendering

---
+++ frontend/__tests__/Item.test.js
@@ -0,0 +1,38 @@
+import ItemComponent from '../components/Item';
+import { shallow } from 'enzyme';
+
+const fakeItem = {
+  id: 'ABC123',
+  title: 'A Cool Item',
+  price: 5000,
+  description: 'This item is really cool!',
+  image: 'dog.jpg',
+  largeImage: 'largedog.jpg',
+};
+
+describe('<Item />', () => {
+  it('renders the image properly', () => {
+    const wrapper = shallow(<ItemComponent item={fakeItem} />);
+    const img = wrapper.find('img');
+
+    expect(img.props().src).toBe(fakeItem.image);
+    expect(img.props().alt).toBe(fakeItem.title);
+  });
+
+  it('renders the price tag and title properly', () => {
+    const wrapper = shallow(<ItemComponent item={fakeItem} />);
+    const PriceTag = wrapper.find('PriceTag');
+
+    expect(PriceTag.children().text()).toBe('$50');
+    expect(wrapper.find('Title a').text()).toBe(fakeItem.title);
+  });
+
+  it('renders out the buttons properly', () => {
+    const wrapper = shallow(<ItemComponent item={fakeItem} />);
+    const buttonsList = wrapper.find('.buttonList');
+    expect(buttonsList.children()).toHaveLength(3);
+    expect(buttonsList.find('Link')).toHaveLength(1);
+    expect(buttonsList.find('AddToCart').exists()).toBe(true);
+    expect(buttonsList.find('DeleteItem').exists()).toBe(true);
+  });
+});
