700071e39398a3aa995509b739f490077ec04da9
literat <tomas@litera.me>
2019-06-27 14:51:02 +0200

    Testing Order Components

--- frontend/components/TakeMyMoney.js
+++ frontend/components/TakeMyMoney.js
@@ -30,8 +30,6 @@ function totalItems(cart) {
 class TakeMyMoney extends React.Component {
   onToken = async (response, createOrder) => {
     NProgress.start();
-    console.log('On Token Called!');
-    console.log(response);
     // manually call the mutation once we have the stripe token
     const order = await createOrder({
       variables: {
@@ -48,32 +46,37 @@ class TakeMyMoney extends React.Component {
   render() {
     return (
       <User>
-        {({ data: { me } }) => (
-          <Mutation
-            mutation={CREATE_ORDER_MUTATION}
-            refetchQueries={[{ query: CURRENT_USER_QUERY }]}
-          >
-            {createOrder => (
-              <StripeCheckout
-                amount={calcTotalPrice(me.cart)}
-                name="Sick Fits"
-                description={`Order of ${totalItems(me.cart)} items!`}
-                image={
-                  me.cart.length && me.cart[0].item && me.cart[0].item.image
-                }
-                stripeKey="pk_test_U14ioBw6xxpeuipRvQlXBrTM00WMliJkBf"
-                currency="USD"
-                email={me.email}
-                token={response => this.onToken(response, createOrder)}
-              >
-                {this.props.children}
-              </StripeCheckout>
-            )}
-          </Mutation>
-        )}
+        {({ data: { me }, loading }) => {
+          if (loading) return null;
+
+          return (
+            <Mutation
+              mutation={CREATE_ORDER_MUTATION}
+              refetchQueries={[{ query: CURRENT_USER_QUERY }]}
+            >
+              {createOrder => (
+                <StripeCheckout
+                  amount={calcTotalPrice(me.cart)}
+                  name="Sick Fits"
+                  description={`Order of ${totalItems(me.cart)} items!`}
+                  image={
+                    me.cart.length && me.cart[0].item && me.cart[0].item.image
+                  }
+                  stripeKey="pk_test_U14ioBw6xxpeuipRvQlXBrTM00WMliJkBf"
+                  currency="USD"
+                  email={me.email}
+                  token={response => this.onToken(response, createOrder)}
+                >
+                  {this.props.children}
+                </StripeCheckout>
+              )}
+            </Mutation>
+          );
+        }}
       </User>
     );
   }
 }
 
 export default TakeMyMoney;
+export { CREATE_ORDER_MUTATION };
