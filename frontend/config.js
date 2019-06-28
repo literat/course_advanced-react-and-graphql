caaebfcfc907833d7eee5579ff5e61455314e0a3
literat <tomas@litera.me>
2019-06-28 11:04:10 +0200

    feat: Add zeit now.sh urls

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4444`;
-export const prodEndpoint = `https://literat-sick-fits-yoga-prod.herokuapp.com/`;
+export const prodEndpoint = `https://sick-fits-backend-wspygfzcmb.now.sh/`;
 export const perPage = 4;
