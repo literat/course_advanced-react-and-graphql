36c9e419696c7b532e2a3c2e87cbde39b0c8a2eb
literat <tomas@litera.me>
2020-12-01 22:21:38 +0100

    fixup! refactor: Rename direcotry with statics to `public`

--- frontend/src/components/Meta.js
+++ frontend/src/components/Meta.js
@@ -4,8 +4,8 @@ const Meta = () => (
   <Head>
     <meta name="viewport" content="width=device-width,initial-scale=1" />
     <meta charSet="utf-8" />
-    <link rel="shortcut icon" href="/public/favicon.png" />
-    <link rel="stylesheet" type="text/css" href="/public/nprogress.css" />
+    <link rel="shortcut icon" href="/favicon.png" />
+    <link rel="stylesheet" type="text/css" href="/nprogress.css" />
     <title>Sick Fits!</title>
   </Head>
 );
