6b3aa82d74ad8290103f9ac953d21828631ee43f
literat <tomas@litera.me>
2020-12-01 23:05:02 +0100

    fixup! refactor: Migrate to Prisma 2 with GraphQL API route

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `/api/graphql`;
-export const prodEndpoint = `https://sick-fits-backend-wspygfzcmb.now.sh/`;
+export const prodEndpoint = `/api/graphql`;
 export const perPage = 4;
--- frontend/src/lib/middleware/useUser.js
+++ frontend/src/lib/middleware/useUser.js
@@ -3,24 +3,15 @@ import db from '../db';
 // 2. Create a middleware that populates the user on each request
 const userMiddleware = () => async (request, response, next) => {
   // if they are not logged in, skip this
-  if (!request.userId) {
-    next();
-  }
-
-  console.log(request.userId);
-
-  const user = await db.user.findUnique(
-    {
+  if (request.userId) {
+    const user = await db.user.findUnique({
       where: {
         id: request.userId,
       },
-    }
-    // '{id, permissions, email, name}'
-  );
+    });
 
-  console.log({ user });
-
-  request.user = user;
+    request.user = user;
+  }
 
   next();
 };
